6377eabc5dff18223895d4a6c8d8c41af75a71e3
greyfd <greysonfox6464@gmail.com>
2025-01-28 09:49:06 -0600

    Added Increase X Speed Button

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,8 @@ public class Main {
         Arena panel = new Arena();
         JPanel controls = new JPanel();
         JButton freezeButton = new JButton("Freeze Balls");
+        JButton increaseXSpeedButton = new JButton("Increase X Speed");
+
         //Modify Elements
         panel.setPreferredSize(new Dimension(400,400));
         freezeButton.addActionListener(e -> {
@@ -24,7 +26,15 @@ public class Main {
                 }
             }
         });
+        increaseXSpeedButton.addActionListener(e -> {
+            //Freeze Action
+            Ball[] balls = panel.getBalls();
+            for (Ball ball : balls) {
+                    ball.setXSpeed(ball.getXSpeed() + 1);
+            }
+        });
         controls.add(freezeButton);
+        controls.add(increaseXSpeedButton);
         controls.setSize(400, 20);
         frame.setLayout(new BorderLayout());
 
